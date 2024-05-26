17dcdb09ac67b815ec2204088d70ebeae77dc5ed
Vishal21121 <vs84077000@gmail.com>
2024-05-26 17:46:36 +0530

    added: added SpeedInsights of vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,20 @@ import RoomJoin from "./components/RoomJoin";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GameBoard from "./components/GameBoard";
 import { Toaster } from "react-hot-toast";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Toaster position="top-right" reverseOrder={false} />
-      <Routes>
-        <Route path="/" element={<RoomJoin />} />
-        <Route path="/room/:roomId" element={<GameBoard />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Toaster position="top-right" reverseOrder={false} />
+        <Routes>
+          <Route path="/" element={<RoomJoin />} />
+          <Route path="/room/:roomId" element={<GameBoard />} />
+        </Routes>
+      </BrowserRouter>
+      <SpeedInsights />
+    </>
   );
 }
 
